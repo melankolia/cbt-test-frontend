da41ec552bd86addc6a728fd22c83a4383bd37e5
Ageng Setyo Nugroho <setyo.nugroho96@gmail.com>
2021-07-26 20:49:46 +0700

    [CBT Test] Install Tailwind css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from "./router";
 import store from "./store";
 import "ant-design-vue/dist/antd.css";
 
+// Tailwind CSS
+import "@/assets/css/tailwind.css";
+
 // Vue Mixin
 import "./mixins";
 
--- src/views/LandingPage/index.vue
+++ src/views/LandingPage/index.vue
@@ -7,7 +7,12 @@
     <p style="padding: 0px 20px 0px 20px; margin: 0">
       Terapi Kognitif Perilaku
     </p>
-    <img width="70%" src="@/assets/images/landing-page.jpg" alt="logo" />
+    <img
+      class="mx-auto"
+      width="70%"
+      src="@/assets/images/landing-page.jpg"
+      alt="logo"
+    />
     <p style="padding: 0px 20px 0px 20px; margin: 0">Tahun 2021</p>
     <p style="padding: 0px 20px 0px 20px; margin: 0">Created By sri maryatun</p>
   </div>
