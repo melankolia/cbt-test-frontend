9fe82534aee37f504e7a09c06e4354fb150ca9e1
Ageng Setyo Nugroho <setyo.nugroho96@gmail.com>
2021-10-02 10:28:40 +0700

    [Prolog] Adjust Label

--- src/views/Quiz/CBT/SectionPractice/Prolog.vue
+++ src/views/Quiz/CBT/SectionPractice/Prolog.vue
@@ -2,10 +2,10 @@
   <div class="my-4">
     <div class="text-center border border-gray-300 rounded-xl p-4">
       <p class="font-airbnb-light mb-4">
-        Terapi Awal CBT merupakan tahapan akhir dari Cognitive Behavior Therapy
-        (CBT) yang merupakan pembuktian kepada diri sendiri dan orang lain
-        terhadap pikiran positif anda sebagai pikiran baru yang diwujudkan dalam
-        tindakan (perilaku) aktivitas sehari-hari <br />
+        Aplikasi latihan CBT merupakan tahapan akhir dari Cognitive Behavior
+        Therapy (CBT) yang merupakan pembuktian kepada diri sendiri dan orang
+        lain terhadap pikiran positif anda sebagai pikiran baru yang diwujudkan
+        dalam tindakan (perilaku) aktivitas sehari-hari <br />
       </p>
       <p class="text-center">
         "Anda Berharga karena anda bergerak dan bermanfaat"
